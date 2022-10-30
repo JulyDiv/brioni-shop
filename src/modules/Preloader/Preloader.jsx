2097a9fd00f17abb4f5a3d59b45627cde40b85fc
JulyDiv <july_developer@list.ru>
2022-10-30 19:40:36 +0700

    сверстала прелоадер и подключила

--- src/modules/Preloader/Preloader.jsx
+++ src/modules/Preloader/Preloader.jsx
@@ -1,13 +1,27 @@
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
+import { useState, useEffect } from "react";
 
 export const Preloader = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 4000);
+  }, []);
+
   return (
-    <div class="preloader">
-      <div class="preloader-wrapper">
-        <img class="preloader__img" src="img/preloader-brioni.png" alt="" />
-        <span class="preloader__span">Continental Style</span>
-      </div>
-    </div>
+    <>
+      {isLoading ? (
+        <div class="preloader">
+          <div class="preloader-wrapper">
+            <img class="preloader__img" src="img/preloader-brioni.png" alt="" />
+            <span class="preloader__span">Continental Style</span>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+    </>
   );
 };
