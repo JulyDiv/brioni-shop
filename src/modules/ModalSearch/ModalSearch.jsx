b02b60760f68bbf7c6ef6f8cb0503ea5156ddd65
JulyDiv <july_developer@list.ru>
2022-12-10 20:17:51 +0700

    ...

--- src/modules/ModalSearch/ModalSearch.jsx
+++ src/modules/ModalSearch/ModalSearch.jsx
@@ -1,26 +1,25 @@
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
 
-export const ModalSearch = ({ isSearch, setIsSearch }) => {
+export const ModalSearch = ({ setIsSearch }) => {
   return (
     <div className="search search--mobile">
       <div className="search-wrapper">
-        
         <form action="#" className="search-form">
           <button
-          type="submit"
-          className="search__button search__button--mobile_visible"
-        ></button>
+            type="submit"
+            className="search-form__button search-form__button-search"
+          ></button>
           <input
             type="text"
             className="search-form__input"
             placeholder="Search"
           />
           <button
-            className="search-form__button"
+            className="search-form__button search-form__button-close"
             onClick={() => setIsSearch(false)}
           >
-            <img src="/img/close-search.svg" alt="close" />
+            {/* <img src="/img/close-search.svg" alt="close" /> */}
           </button>
         </form>
       </div>
--- src/modules/SwiperCard/SwiperCardSlide.jsx
+++ src/modules/SwiperCard/SwiperCardSlide.jsx
@@ -2,15 +2,24 @@
 import React, { useState, useContext } from "react";
 import { SwiperSlide } from "swiper/react";
 import { AppContext } from "../../context/AppContext";
+import { SelectModal } from "../SelectModal/SelectModal";
 
 export const SwiperCardSlide = ({ slide, jacket }) => {
   const { count, selects, setSelects } = useContext(AppContext);
+
+  const [isSelectModal, setIsSelectModal] = useState(false);
   const [activeSelect, setActiveSelect] = useState(false);
-    const select = {
-      ...selects,
-      ...jacket
-      //count: count.count,
-    };
+
+  const select = {
+    ...selects,
+    ...jacket,
+    count: count.count,
+  };
+
+  /*если сначала добавить пиджак, то добавляются и слайдеры,
+    если сначала добавить слайдер, а потом добавить пиджак,
+    то пиджак выместит слайдер и встанет на его место*/
+
   const addSelect = () => {
     const list = new Map(selects);
     if (list.get(slide.id)) {
@@ -23,28 +32,41 @@ export const SwiperCardSlide = ({ slide, jacket }) => {
       list.set(slide.id, select);
     }
     setSelects(list);
-    //console.log(list);
-    //console.log(jacket);
+
     setActiveSelect(true);
-    activeSelect ? setActiveSelect(false) : setActiveSelect(true);
-    //addSelect();
+    //activeSelect ? setActiveSelect(false) : setActiveSelect(true);
+    isSelectModal ? setIsSelectModal(false) : setIsSelectModal(true);
   };
-  //console.log(slide);
+
   return (
-    <SwiperSlide key={slide.id} className="swiper-card__slide">
-      <img src={slide.img} className="swiper-card__image" alt="Image: Jacket" />
-      <div className="swiper-card__select">
-        <span className="swiper-card__text">{slide.name}</span>
-        <button
-          className={
-            activeSelect
-              ? "swiper-card__icon swiper-card__icon--active"
-              : "swiper-card__icon"
-          }
-          onClick={() => addSelect()}
-        ></button>
-      </div>
-      <span className="swiper-card__price">{slide.price}</span>
-    </SwiperSlide>
+    <>
+      <SwiperSlide key={slide.id} className="swiper-card__slide">
+        <img
+          src={slide.img}
+          className="swiper-card__image"
+          alt="Image: Jacket"
+        />
+        <div className="swiper-card__select">
+          <span className="swiper-card__text">{slide.name}</span>
+          <button
+            className={
+              activeSelect
+                ? "swiper-card__icon swiper-card__icon--active"
+                : "swiper-card__icon"
+            }
+            onClick={() => addSelect()}
+          ></button>
+        </div>
+        <span className="swiper-card__price">{slide.price}</span>
+      </SwiperSlide>
+      {isSelectModal && (
+        <SelectModal
+          jacket={jacket}
+          addSelect={addSelect}
+          title="Add to Select"
+          button="Select"
+        />
+      )}
+    </>
   );
 };
